0562a3510dd712d6b1ff0f3b9b7499f65732ee49
philipreese <pbreese42@gmail.com>
2025-07-03 12:40:01 -0400

    create PUT /api/users endpoint to allow users to update their own username and password

--- handler_users.go
+++ handler_users.go
@@ -18,14 +18,14 @@ type User struct {
 	Password  string    `json:"-"`
 }
 
-func (cfg *apiConfig) handlerCreateUser(writer http.ResponseWriter, req *http.Request) {
-	type createUserRequest struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
+type userRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
+func (cfg *apiConfig) handlerCreateUser(writer http.ResponseWriter, req *http.Request) {
 	decoder := json.NewDecoder(req.Body)
-	var userRequest createUserRequest
+	var userRequest userRequest
 	if err := decoder.Decode(&userRequest); err != nil {
 		respondWithError(writer, http.StatusInternalServerError, "Couldn't decode parameters: " + err.Error())
 		return
@@ -53,4 +53,48 @@ func (cfg *apiConfig) handlerCreateUser(writer http.ResponseWriter, req *http.Re
 		Email: dbUser.Email,
 	}
 	respondWithJSON(writer, http.StatusCreated, user)
+}
+
+func (cfg *apiConfig) handlerUpdateUser(writer http.ResponseWriter, req *http.Request) {
+	tokenString, err := auth.GetBearerToken(req.Header)
+	if err != nil {
+		respondWithError(writer, http.StatusUnauthorized, "Couldn't get bearer token: " + err.Error())
+		return
+	}
+
+	userID, err := auth.ValidateJWT(tokenString, cfg.tokenSecret)
+	if err != nil {
+		respondWithError(writer, http.StatusUnauthorized, "Couldn't validate JWT: " + err.Error())
+		return
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	var userRequest userRequest
+	if err := decoder.Decode(&userRequest); err != nil {
+		respondWithError(writer, http.StatusInternalServerError, "Couldn't decode parameters: " + err.Error())
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(userRequest.Password)
+	if err != nil {
+		respondWithError(writer, http.StatusInternalServerError, "Couldn't hash password: " + err.Error())
+		return
+	}
+
+	user, err := cfg.db.UpdateUser(req.Context(), database.UpdateUserParams{
+		ID: userID,
+		Email: userRequest.Email,
+		HashedPassword: hashedPassword,
+	})
+	if err != nil {
+		respondWithError(writer, http.StatusInternalServerError, "Failed to update user: " + err.Error())
+		return
+	}
+
+	respondWithJSON(writer, http.StatusOK, User{
+		ID: user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email: user.Email,
+	})
 }
--- main.go
+++ main.go
@@ -31,12 +31,11 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
 	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
-
 	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
-
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUpdateUser)
 
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerGetChirps)
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerGetChirpByID)
